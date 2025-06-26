b5cc20c43dcffaab1af8911f775071b92806d027
shim079 <150309430+shim079@users.noreply.github.com>
2025-06-26 12:40:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('#menu-icon');
+let navbar =document.querySelector('navbar');
+
+menu.onclick =() =>{
+menu.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+
+}
+window.onscroll =() =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+      strings: [' FrontEnd Developer', 'BackEnd Developer' ,'Content Creator' ,'Graphic Designer'],
+      typeSpeed: 80,
+      backSpeed: 80,
+      backdelay: 1200,
+      loop: true,
+    });
+
+    
+function showSidebar() {
+const side = document.querySelector('.sidebar')
+side.style.display = 'flex'
+}
+
+function closeSidebar() {
+    const side = document.querySelector('.sidebar')
+side.style.display = 'none'
+}
+
+    
